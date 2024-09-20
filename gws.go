ab40c947d6936f212437638d902bb493b86fdf74
julianp829 <julianpala@lewisu.edu>
2024-09-20 13:17:59 -0500

    Update all files

--- gws.go
+++ gws.go
@@ -5,17 +5,21 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+
+	"github.com/julianp829/GoWebServer_/hello"
+	"github.com/julianp829/GoWebServer_/help"
+	"github.com/julianp829/GoWebServer_/syllabus"
 )
 
 func main() {
-	http.HandleFunc("/hello-world", HelloWorld)
-	http.HandleFunc("/hello-world-html", HelloWorldHTML)
-	http.HandleFunc("/hello-world-json", HelloWorldJSON)
-	http.HandleFunc("/syllabus", SyllabusHandler)
-	http.HandleFunc("/help", Help)
+	http.HandleFunc("/hello-world", hello.HelloWorld)
+	http.HandleFunc("/hello-world-html", hello.HelloWorldHTML)
+	http.HandleFunc("/hello-world-json", hello.HelloWorldJSON)
+	http.HandleFunc("/syllabus", syllabus.SyllabusHandler)
+	http.HandleFunc("/help", help.Help)
 
 	// Start server
-	fmt.Println("Starting server on :8080")
+	fmt.Println("Starting server on http://localhost:8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		fmt.Println("Error starting server:", err)
 		os.Exit(1)
